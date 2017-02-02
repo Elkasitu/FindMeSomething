34388c3d6714ba3cadb8ec002ab8ce98443c81d8
Adrian Torres <atorresj@hotmail.be>
2017-02-02 17:32:19 +0100

    Add Google Play Services, preparatory code for list of findables

--- app/src/main/java/torres/adrian/findmesomething/MainActivity.java
+++ app/src/main/java/torres/adrian/findmesomething/MainActivity.java
@@ -1,13 +1,28 @@
 package torres.adrian.findmesomething;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity
+{
+	private String[] findablesArray = {"Restaurants", "Banks/ATMs", "Night Shops",
+									   "Night Clubs", "Parks", "Parkings", "Public Transport"};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private ListView findableListView;
+	private ArrayAdapter arrayAdapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		findableListView = (ListView) findViewById(R.id.findableList);
+
+		arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, findablesArray);
+		findableListView.setAdapter(arrayAdapter);
+	}
 }
